ab3c9bf89b98cc8c5ea7c0f9617f2c4c3e7c7f7d
gaurav033 <59844505+gaurav033@users.noreply.github.com>
2026-08-15 20:57:12 +0530

    Update: top-k-elements-in-list - submission-2

--- Data Structures & Algorithms/top-k-elements-in-list/submission-2.java
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.java
@@ -1,35 +1,35 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
         }
-        HashMap<Integer,List<Integer>> revMap = new HashMap<>();
-        for(Integer i:map.keySet()){
-            List<Integer> tempList = revMap.getOrDefault(map.get(i),new ArrayList<Integer>());
-            tempList.add(i);
-            revMap.put(map.get(i),tempList);
+
+        List<Integer>[] buckets = new List[nums.length+1];
+
+        for(int i:map.keySet()){
+            int freq = map.get(i);
+            if(buckets[freq]==null){
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(i);
         }
+
         int[] result = new int[k];
         int count = 0;
-        for (int frequency = nums.length; frequency >= 1; frequency--) {
-            if(count==k){
-                break;
-            }
-            if (revMap.containsKey(frequency)) {
-                List<Integer> tempList = revMap.get(frequency);
-                
-                for(Integer i:tempList){
+
+        for(int i=buckets.length-1;i>=0 && count<k;i--){
+            if(buckets[i]!=null){
+                for(int num:buckets[i]){
+                    result[count] = num;
+                    count++;
+
                     if(count==k){
                         break;
                     }
-                    result[count] = i;
-                    count++;
                 }
-                
             }
         }
-        
         return result;
     }
 }
